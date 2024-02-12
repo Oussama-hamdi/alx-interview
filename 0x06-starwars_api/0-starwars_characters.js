8db3b180841e658b79bef74c945aee05b2404b17
Oussama-hamdi <ohamdi776@gmail.com>
2024-02-12 06:05:19 -0800

    Fixed semistandard problem

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* Prints all characters of a Star Wars movie */
 
-const request = require("request");
+const request = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
