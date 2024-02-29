dd677a81a80f225bff68398d0718aebfef0634fb
Oussama-hamdi <ohamdi776@gmail.com>
2024-02-29 04:36:44 -0800

    Fixed efficiency problem

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,11 +6,14 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    coins.sort(reverse=True)
+    num_coins = 0
 
     for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+        while coin <= total:
+            total -= coin
+            num_coins += 1
+        if total == 0:
+            return num_coins
 
-    return dp[total] if dp[total] != float('inf') else -1
+    return -1
